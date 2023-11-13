05ac038e0da9324c5f71914ed77e2cbb4fe7f09d
hayannn <dlgkdis801@naver.com>
2023-11-13 23:26:02 +0900

    feat(InputView): 주문할 메뉴의 이름 및 개수 입력 요청 문구 출력 기능 구현

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -8,4 +8,7 @@ public class InputView {
         String input = Console.readLine();
         return Integer.parseInt(input.trim());
     }
+    public static String readOrderedMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
 }
