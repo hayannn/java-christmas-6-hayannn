bf8293f3960a893a99ec7c36ff7e2e169c1b0597
hayannn <dlgkdis801@naver.com>
2023-11-13 23:51:24 +0900

    feat(OutputView): 이벤트 플레너의 결과를 출력하기 위한 날짜 포함 문구 출력 기능 구현

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,7 +1,24 @@
 package christmas.View;
 
+import christmas.Domain.Order;
+import christmas.Util.EventUtil;
+import java.util.List;
+import java.util.Optional;
+
 public class OutputView {
     public static void printGreeting() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printError1(String errorMessage) {
+        System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    public static void printError2(String errorMessage) {
+        System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    public static void printOrderSummary(OrderService orderService) {
+        System.out.println(orderService.getFormattedSelectedDate() + "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
     }
 }
