96521fa42bb047ffd678df58febe685913d9c020
hayannn <dlgkdis801@naver.com>
2023-11-14 00:02:51 +0900

    chore(Application): 크리스마스 프로모션이 컨트롤러에서 시작될 수 있도록 하는 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,9 @@
 package christmas;
 
+import christmas.Controller.ChristmasPlannerController;
+
 public class Application {
     public static void main(String[] args) {
-
+        new ChristmasPlannerController().run();
     }
 }
