e91bfa29bd23bbbbbfef0bc09cc5724095c91711
hayannn <dlgkdis801@naver.com>
2023-11-13 23:01:02 +0900

    feat(Application): 크리스마스 프로모션이 컨트롤러에서 시작되도록 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,23 @@
 package christmas;
 
+import christmas.Controller.ChristmasPlannerController;
+import christmas.Repository.EventBenefitRepository;
+import christmas.Service.ChristmasPlannerService;
+import christmas.View.InputView;
+import christmas.View.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            EventBenefitRepository eventBenefitRepository = new EventBenefitRepository(); // 적절한 생성자로 초기화
+            ChristmasPlannerService christmasPlannerService = new ChristmasPlannerService(eventBenefitRepository);
+            InputView inputView = new InputView();
+            OutputView outputView = new OutputView();
+
+            ChristmasPlannerController christmasPlannerController = new ChristmasPlannerController(christmasPlannerService, inputView, outputView);
+            christmasPlannerController.processEventPlanning();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
