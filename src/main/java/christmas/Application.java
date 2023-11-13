e57225058d3e3f29730005f7e246efcdd347d16a
hayannn <dlgkdis801@naver.com>
2023-11-13 23:13:19 +0900

    remove: 코드 재구성을 위한 기존 코드 전체 삭제

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,23 +1,7 @@
 package christmas;
 
-import christmas.Controller.ChristmasPlannerController;
-import christmas.Repository.EventBenefitRepository;
-import christmas.Service.ChristmasPlannerService;
-import christmas.View.InputView;
-import christmas.View.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        try {
-            EventBenefitRepository eventBenefitRepository = new EventBenefitRepository(); // 적절한 생성자로 초기화
-            ChristmasPlannerService christmasPlannerService = new ChristmasPlannerService(eventBenefitRepository);
-            InputView inputView = new InputView();
-            OutputView outputView = new OutputView();
 
-            ChristmasPlannerController christmasPlannerController = new ChristmasPlannerController(christmasPlannerService, inputView, outputView);
-            christmasPlannerController.processEventPlanning();
-        } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-        }
     }
 }
