0e2c343678c2abeb50a14a963817ef030b61e833
hayannn <dlgkdis801@naver.com>
2023-11-13 23:52:29 +0900

    feat(OutputView): 이벤트 플레너의 혜택 및 할인, 배지를 출력하는 기능 구현

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -20,5 +20,59 @@ public class OutputView {
 
     public static void printOrderSummary(OrderService orderService) {
         System.out.println(orderService.getFormattedSelectedDate() + "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+
+        printMenu(orderService.getOrderedMenuList());
+        printTotalPriceBeforeDiscount(orderService.getTotalPriceBeforeDiscount());
+        printGiftMenu(orderService.getGiftMenu());
+        printAppliedEvents(orderService.getAppliedEvents());
+        printTotalDiscountAmount(orderService.getTotalDiscountAmount());
+        printTotalPriceAfterDiscount(orderService);
+        printBadge(orderService.getTotalDiscountAmount());
+    }
+
+    private static void printMenu(List<Order> orderedMenuList) {
+        System.out.println("<주문 메뉴>");
+        orderedMenuList.forEach(
+                order -> System.out.println(order.getMenu().getName() + " " + order.getQuantity() + "개"));
+        System.out.println();
+    }
+
+    private static void printTotalPriceBeforeDiscount(int totalPriceBeforeDiscount) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(String.format("%,d원\n", totalPriceBeforeDiscount));
+    }
+
+    private static void printGiftMenu(Optional<String> giftMenu) {
+        System.out.println("<증정 메뉴>");
+        System.out.println(giftMenu.orElse("없음") + "\n");
+    }
+
+    private static void printAppliedEvents(List<Event> appliedEvents) {
+        System.out.println("<혜택 내역>");
+
+        if (appliedEvents.isEmpty()) {
+            System.out.println("없음");
+        }
+
+        appliedEvents.forEach(event -> System.out.println(event.getEventDescription()));
+
+        System.out.println();
+    }
+
+
+    private static void printTotalDiscountAmount(int totalDiscountAmount) {
+        System.out.println("<총혜택 금액>");
+        System.out.println(String.format("-%,d원\n", totalDiscountAmount));
+    }
+
+    private static void printTotalPriceAfterDiscount(OrderService orderService) {
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(String.format("%,d원\n", orderService.getTotalPriceAfterDiscount()));
+    }
+
+
+    private static void printBadge(int totalDiscountAmount) {
+        System.out.println("<12월 이벤트 배지>");
+        System.out.println(EventUtil.calculateBadge(totalDiscountAmount) + "\n");
     }
 }
