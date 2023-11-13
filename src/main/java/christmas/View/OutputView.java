3e1c1d9cd1f489d7d073ad2d00b7f9c2cd5073fa
hayannn <dlgkdis801@naver.com>
2023-11-13 22:32:20 +0900

    feat(OutputView): 고객이 입력한 날짜를 반영한 이벤트 혜택 미리보기 문구 출력 기능 구현

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,7 +1,13 @@
 package christmas.View;
 
+import java.util.List;
+
 public class OutputView {
     public static void printEventPlannerGreeting() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+    public static void printOrderPreview(int date, List<Order> orders) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n%n", date);
+        printOrderMenu(orders);
     }
 }
